bb290088ddc4377e517d70c9ffd3ca92d2d262fa
Gaudy035 <miloszgaudy@gmail.com>
2026-04-07 23:52:10 +0200

    Fixed dependency conflicts, added Swagger Docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MovieRate docs')
+    .setDescription('Swagger docs for MovieRate')
+    .setVersion('1.0')
+    .addTag('movies')
+    .addBearerAuth()
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, doc);
 
   app.enableCors();
 
